8bc62c27069c0ecbcae750e15d39ade5d1060d2d
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-05-28 11:13:34 +0300

    Support setting schedule for local report purge
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- manifests/reports_purge.pp
+++ manifests/reports_purge.pp
@@ -1,10 +1,14 @@
 # @summary clean up old yaml reports
 #
-class puppetmaster_common::reports_purge {
+class puppetmaster_common::reports_purge
+(
+  Integer $max_days = 7
+)
+{
 
   # Remove old reports to prevent disk space from filling up
   cron { 'remove-old-reports':
-    command => '/usr/bin/find /opt/puppetlabs/server/data/puppetserver/reports/ -type f -mtime +7 -delete',
+    command => "/usr/bin/find /opt/puppetlabs/server/data/puppetserver/reports/ -type f -mtime +${max_days} -delete",
     user    => 'root',
     hour    => 2,
     minute  => 0,
